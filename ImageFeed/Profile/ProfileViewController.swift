b5ba020bc1107596cbd42cb372d883f9b13b8ff2
Gabsik <gabsik1998@mail.ru>
2025-03-12 21:22:08 +0300

    Remove storyboard files and implement profile screen in code

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,97 @@
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    let imageViewProfile = UIImageView()
+    let nameLabel = UILabel()
+    let loginNameLabel = UILabel()
+    let descriptionLabel = UILabel()
+    let logoutButton = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupImageView()
+        setupNameLabel()
+        setupLoginNameLabel()
+        setupDescriptionLabel()
+        setupLogoutButton()
+    }
+    
+    private func setupImageView() {
+        let profilImage = UIImage(named: "avatar")
+        imageViewProfile.image = profilImage
+        
+        view.addSubview(imageViewProfile)
+        
+        imageViewProfile.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            imageViewProfile.widthAnchor.constraint(equalToConstant: 70),
+            imageViewProfile.heightAnchor.constraint(equalToConstant: 70),
+            imageViewProfile.topAnchor.constraint(equalTo: view.topAnchor, constant: 76),
+            imageViewProfile.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
+        ])
+    }
+    
+    private func setupNameLabel() {
+        nameLabel.text = "Екатерина Новикова"
+        nameLabel.font = UIFont.systemFont(ofSize: 23)
+        nameLabel.textColor = UIColor(named: "YP White (iOS)")
+        
+        view.addSubview(nameLabel)
+        
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            nameLabel.topAnchor.constraint(equalTo: imageViewProfile.bottomAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
+        ])
+        
+    }
+    
+    private func setupLoginNameLabel() {
+        loginNameLabel.text = "@ekaterina_nov"
+        loginNameLabel.font = UIFont.systemFont(ofSize: 13)
+        loginNameLabel.textColor = UIColor(named: "YP Gray (iOS)")
+        
+        view.addSubview(loginNameLabel)
+        
+        loginNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            loginNameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            loginNameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
+        ])
+    }
+    
+    private func setupDescriptionLabel() {
+        descriptionLabel.text = "Hello, world!"
+        descriptionLabel.font = UIFont.systemFont(ofSize: 13)
+        descriptionLabel.textColor = UIColor(named: "YP White (iOS)")
+        
+        view.addSubview(descriptionLabel)
+        
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            descriptionLabel.topAnchor.constraint(equalTo: loginNameLabel.bottomAnchor, constant: 8),
+            descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
+        ])
+    }
+    
+    private func setupLogoutButton() {
+        logoutButton.setImage(UIImage(named: "Exit"), for: .normal)
+        
+        view.addSubview(logoutButton)
+        
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            logoutButton.widthAnchor.constraint(equalToConstant: 44),
+            logoutButton.heightAnchor.constraint(equalToConstant: 44),
+            logoutButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 99),
+            logoutButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24)
+        ])
+    }
+}
