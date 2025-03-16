8a0eaa443d17b5284dad9cdf2eb1b751105043a7
Gabsik <gabsik1998@mail.ru>
2025-03-16 11:27:30 +0300

    added Constants file

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,10 @@
+import Foundation
+import UIKit
+
+enum Constants {
+    static let accessKey = "HSpCrBKKEbC5RXExUy_1b2ATWpP609ge5CL7XQfhjxY"
+    static let secretKey = "usIflqaMxWwYp9RnzR00ts292rP_NyGMBHIcmXQ8VsU"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com/")
+}
